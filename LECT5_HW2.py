8d550f543e40d5eb40ce86ed1eeacb6fe67f6115
Yulia Magonya <ymagonia@gmail.com>
2024-05-01 01:45:44 +0300

    just curious

--- LECT5_HW2.py
+++ LECT5_HW2.py
@@ -1,14 +1,21 @@
 import math
 
 ls_t = [2, 4, 6, 2, 1, 1, 9, 4, 6]
+# ls_t = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 MIN = 3
 MAX = 6
 
-ls_nn = sum(int(i) for i in ls_t if i >= MIN and i <= MAX)
-print(ls_nn)
+ls_nn = sum(i for i in ls_t if i >= MIN and i <= MAX)
+if ls_nn == 0:
+    print(None)
+else:
+    print(ls_nn)
 
-ls_qq = math.prod(int(i) for i in ls_t if i >= MIN and i <= MAX)
-print(ls_qq)
+ls_qq = math.prod(i for i in ls_t if i >= MIN and i <= MAX)
+if ls_qq == 0:
+    print(None)
+else:
+    print(ls_qq)
 
 
 
