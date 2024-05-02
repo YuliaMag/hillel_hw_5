f0b54eafaf107696cf5f22bcb8eb9bad5a11534a
Yulia Magonya <ymagonia@gmail.com>
2024-05-02 23:59:07 +0300

    comm_n2.3

--- LECT5_HW2.py
+++ LECT5_HW2.py
@@ -5,34 +5,34 @@
 # ls_t = [1, -1]
 # ls_t = [-1, 1]
 # ls_t = [1, 1, 1, 1, 1]
+# ls_t = [4, 0]
 
 ls_t = [2, 4, 6, 2, 1, 1, 9, 4, 6]
 
-
 MIN = 3
 MAX = 6
+# MIN = -30
+# MAX = 60
 
-sum_1 = 0
+sum_1 = None
 for i in ls_t:
     if i >= MIN and i <= MAX:
-        sum_1 += i
-if sum_1 != 0:
-    print(sum_1)
-else:
-    print(None)
+        if sum_1 is None:
+            sum_1 = i
+        else:
+            sum_1 += i
+print(sum_1)
 
 
-prd = 0
+prd = None
 for i in ls_t:
     if i >= MIN and i <= MAX:
-        if prd == 0:
+        if prd is None:
             prd = i
         else:
             prd *= i
-if prd != 0:
-    print(prd)
-else:
-    print(None)
+print(prd)
+
 
 
 
